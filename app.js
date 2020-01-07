5f979307aa413ddebf6afd1c098fb0904b0a5c9d
Edward Stern <edwardstern242@gmail.com>
2020-01-07 16:36:22 +0000

    in middle of function to make box shadow after click

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ const side1Output = document.getElementById('side-1')
 const side2Output = document.getElementById('side-2')
 const addButton = document.getElementById('add-button')
 
-dinnerButton.onclick = function (){
+dinnerButton.onclick = () => {
     changeBorder()
+    setTimeout(() => {handleDinnerButton()}, 200);
+    setTimeout(() => {dinnerButton.setAttribute('class', 'dinner-button-pressed'), 1000})
+}
+
+function handleDinnerButton (){
     mainOutput.innerHTML = 'here is your main'
     side1Output.innerHTML = 'here is your first side'
     side2Output.innerHTML = 'here is your second side'
