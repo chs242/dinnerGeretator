090230131585932651ef53fffc09a06d4629cc7f
Edward Stern <edwardstern242@gmail.com>
2020-01-07 14:57:13 +0000

    added add button

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const output = document.getElementById('output')
 const mainOutput = document.getElementById('main')
 const side1Output = document.getElementById('side-1')
 const side2Output = document.getElementById('side-2')
+const addButton = document.getElementById('add-button')
 
 dinnerButton.onclick = function (){
-    setTimeout(()=>{mainOutput.innerHTML = 'here is your main'}, 1000)
+    changeBorder()
+    mainOutput.innerHTML = 'here is your main'
     side1Output.innerHTML = 'here is your first side'
     side2Output.innerHTML = 'here is your second side'
+}
+
+function changeBorder (){
+    output.setAttribute('class', 'green-border')
+    setTimeout(()=>{output.setAttribute('class', 'output')}, 250) 
 }
