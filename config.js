a90183e25afc50add89ebb8d18a30c31ff040283
Edward Stern <edwardstern242@gmail.com>
2020-01-16 18:54:03 +0000

    last commit

---
+++ config.js
@@ -0,0 +1,11 @@
+   // Your web app's Firebase configuration
+
+   var firebaseConfig = {
+    apiKey: "AIzaSyDri08BIfNtKQLipY4T7bP4F3Dm6r4vKLo",
+    authDomain: "dinner-generator-49191.firebaseapp.com",
+    databaseURL: "https://dinner-generator-49191.firebaseio.com",
+    projectId: "dinner-generator-49191",
+    storageBucket: "dinner-generator-49191.appspot.com",
+    messagingSenderId: "91970655312",
+    appId: "1:91970655312:web:cf77255e90d6da3219ed55"
+};
